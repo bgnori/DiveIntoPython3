75cc8d0ff531dce948fb0fddeddc8190b2d32185
bgnori <bgnori@gmail.com>
2013-07-04 19:34:33 +0900

    chap 3 quiz 9 moved AOJ quiz to forAOJ.txt 	new file:   exercises/chap3/q0009.py 	new file:   forAOJ.txt 	modified:   forChap3.txt 	new file:   get_primes.sh 	new file:   tests/chap3/q0009.py

---
+++ exercises/chap3/q0009.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def is_prime(n):
+
+    #place your implementation here
+    pass
+
+def prime(n):
+
+    #place your implementation here
+    pass
---
+++ get_primes.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# prime list
+curl -l -O http://oeis.org/A000040/a000040.txt
