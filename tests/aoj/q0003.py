4026579ad8cb80b6e5e3de93c1dc4e3cf9331a5d
bgnori <bgnori@gmail.com>
2013-07-04 20:18:08 +0900

    aoj quiz 3 	new file:   exercises/aoj/q0003.py 	new file:   tests/aoj/q0003.py

---
+++ tests/aoj/q0003.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+
+import io
+import unittest
+
+from exercises.aoj import q0003
+
+data = """4
+1 2
+3 4 5
+0 
+2 6 7"""
+
+class TestCase(unittest.TestCase):
+    def test_docstring(self):
+        self.assertIsNotNone(q0003.reader.__doc__)
+
+    def test_0(self):
+        f = io.StringIO('0')
+        xs = q0003.reader(f)
+        self.assertEqual([], xs)
+
+    def test_some(self):
+        f = io.StringIO(data)
+        xs = q0003.reader(f)
+        self.assertEqual([[2], [4, 5, 6], [], [7, 8]], xs)
+
+
+if __name__ == '__main__':
+    unittest.main()
