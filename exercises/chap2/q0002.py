d6881ba611c32a64f47870eb158a658f1f151538
bgnori <bgnori@gmail.com>
2013-07-04 14:46:42 +0900

    modified:   exercises/chap2/q0002.py     sample solution for q0002.py

--- exercises/chap2/q0002.py
+++ exercises/chap2/q0002.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
+
 def fizzbuff(n):
-
-    #place your implementation here
-    pass
-
-
+    '''Fizz Buzz'''
+    if n % 15 == 0:
+        return "Fizz Buzz"
+    elif n % 5 == 0:
+        return "Buzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    else:
+        return n
