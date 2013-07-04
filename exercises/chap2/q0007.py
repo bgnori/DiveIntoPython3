284e73b5877b37f5bd5e4b349ffc92e217109433
bgnori <bgnori@gmail.com>
2013-07-04 15:17:40 +0900

    sample imple. 	modified:   exercises/chap2/q0007.py

--- exercises/chap2/q0007.py
+++ exercises/chap2/q0007.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
 def wordcount(xs):
+    '''simple implementation of wordcount.
+    using collections.Counter is also good idea.
 
-    #place your implementation here
-    pass
+    "d.get(x, 0)" is the pattern should be learned.
+    '''
+    d = {}
+    for x in xs:
+        c = d.get(x, 0) + 1
+        d[x] = c
+    return d
