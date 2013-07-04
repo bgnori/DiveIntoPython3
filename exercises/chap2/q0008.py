3944cfa3ed290d634859765f453c6a0de6ba6b8b
bgnori <bgnori@gmail.com>
2013-07-04 15:20:16 +0900

    modified:   exercises/chap2/q0008.py

--- exercises/chap2/q0008.py
+++ exercises/chap2/q0008.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
 def flatten(xs):
-
-    #place your implementation here
-    pass
+    ''' assume that xs and its items do not reference itself'''
+    
+    r = []
+    for x in xs:
+        if isinstance(x, list):
+            r.extend(flatten(x))
+        else:
+            r.append(x)
+    return r
