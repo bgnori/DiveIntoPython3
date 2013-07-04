c967d8f4a8b2fb873918ed4d351012cf28ff38f0
bgnori <bgnori@gmail.com>
2013-07-04 21:19:07 +0900

    fixed test 	modified:   tests/aoj/q0002.py

--- tests/aoj/q0002.py
+++ tests/aoj/q0002.py
@@ -17,9 +17,9 @@ class TestCase(unittest.TestCase):
         self.assertEqual([], xs)
 
     def test_somedef(self):
-        f = io.StringIO('foo\nbar):\defone\nika\ntako):\ndeftwo')
+        f = io.StringIO('foo\nbar):\ndefone\nika\ntako):\ndeftwo')
         xs = q0002.colonfind(f)
-        self.assertEqual(['bar):', 'tako):'], xs)
+        self.assertEqual(['bar):\n', 'tako):\n'], xs)
 
 if __name__ == '__main__':
     unittest.main()
