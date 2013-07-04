35e9b9a387b7b2078130355e23df584aaa3dd6ca
bgnori <bgnori@gmail.com>
2013-07-04 21:31:54 +0900

    sample impl for quiz 3 of aoj 	modified:   exercises/aoj/q0003.py

--- exercises/aoj/q0003.py
+++ exercises/aoj/q0003.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
 def reader(f):
+    ''' '''
+    xxs = []
+    for i, line in enumerate(f):
+        if i == 0:
+            continue
+        xs = list(map(int, line.split()))[1:]
+        xxs.append(xs)
+    return xxs
 
-    #place your implementation here
-    pass
