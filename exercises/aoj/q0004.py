abfe4717fe91de0f5ed937565b980122e5b83323
bgnori <bgnori@gmail.com>
2013-07-04 21:34:46 +0900

    sample imp for aoj quiz 4 	modified:   exercises/aoj/q0004.py

--- exercises/aoj/q0004.py
+++ exercises/aoj/q0004.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
 def reader(f):
+    ''' '''
+    xxs = []
+    for i, line in enumerate(f):
+        xs = list(map(int, line.split()))
+        if len(xs) == 1 and xs[0] == 0:
+            break
+        xxs.append(xs)
+    return xxs
 
-    #place your implementation here
-    pass
