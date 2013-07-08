48937874c94476118bcdf3ce27a5d0f854b9bb74
bgnori <bgnori@gmail.com>
2013-07-08 15:46:22 +0900

    sample answer for chap 3 quiz5 	modified:   exercises/chap3/q0005.py

--- exercises/chap3/q0005.py
+++ exercises/chap3/q0005.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 def foo(n):
-
-    #place your implementation here
-    pass
+    '''
+    5) Fizz Buzzのリストxsを内包表記を用いてつくる関数foo(n)を実装せよ.
+    0 < n
+    xs[1] = 1, xs[2] = 2, xs[3] ='Fizz', 
+    ... xs[15] = 'Fizz Buzz' ... xs[n] = ??? などとなる.
+    '''
+    return ['Fizz Buzz' if x % (3*5) == 0 else 'Fizz' if x % 3 == 0 else
+            'Buzz' if x % 5 == 0 else x for x in range(n+1)]
